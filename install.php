67d1dafd342665223f654875896d98eedb4f79af
HenryGardiner <gardiner.hcw@oundleschool.org.uk>
2020-10-12 11:59:25 +0100

    end of 9. Sending it to the Database (including install.php)

---
+++ install.php
@@ -0,0 +1,35 @@
+
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password= '';
+//note no Database mentioned here!
+try {
+ $conn = new PDO("mysql:host=$servername", $username, $password);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $sql = "CREATE DATABASE IF NOT EXISTS Options";
+ $conn->exec($sql);
+ //next 3 lines optional only needed really if you want to go on an do more SQL here!
+ $sql = "USE Options";
+ $conn->exec($sql);
+ echo "DB created successfully";
+}
+catch(PDOException $e)
+{
+ echo $sql . "<br>" . $e->getMessage();
+}
+
+include_once("connection.php");
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUser;
+CREATE TABLE TblUser 
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Gender VARCHAR(1) NOT NULL,
+Surname VARCHAR(20) NOT NULL,
+Forename VARCHAR(20) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+House VARCHAR(20) NOT NULL,
+Year INT(2) NOT NULL,
+Role TINYINT(1))");
+$stmt->execute();
+$stmt->closeCursor();
+?>
